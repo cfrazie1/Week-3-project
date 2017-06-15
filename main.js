6af54ab0ea2154920ec1c9b9a46af965b4f019c0
Curtis Frazier <fraziercurtis2003@yahoo.com>
2017-06-15 09:18:05 -0400

    turn in version

--- main.js
+++ main.js
@@ -18,22 +18,17 @@ const clearBtn = document.querySelector(".clearbutton"),
       adderBtn = document.querySelector(".adderBtn");
 
 
-// multiplierBtn.onclick = function() {
-//   console.log("multiplierBtn")
-// };
-// let arithmeticBtns = document.querySelectorAll(".arithmeticBtn")
-// console.log(arithmeticBtns)
+
 
 let buttons = document.querySelectorAll("p");
 
-// console.log(buttons);
+
 console.log(buttons.length)
-//create a for loop to click buttons
-//
+
 for (let i = 0; i < buttons.length; i++){
   let btn = buttons[i];
 
-  //add eventlisterner
+
    btn.addEventListener("click", function(){
 
      if(btn.innerHTML == "=") {
@@ -42,11 +37,7 @@ for (let i = 0; i < buttons.length; i++){
       equation = equation.replace("x", "*");
       calWindow.innerHTML = eval(equation);
 
-       //  console.log(equation)
-       //  1 + 2
-       //  1 / 2
-       //  1 - 1
-       //  1 * 1
+
      } else if(btn.innerHTML == "C") {
        calWindow.innerHTML = "";
 
@@ -58,8 +49,6 @@ for (let i = 0; i < buttons.length; i++){
      }
 
 
-  // var msg = btn.textContent;
-  // console.log("msg",msg)
 
    });
 }
